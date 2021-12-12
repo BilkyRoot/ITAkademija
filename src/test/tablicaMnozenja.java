da1a7cd883d732ba664b2bea5bdd6e7bdf6e8b74
BilkyRoot <bilalsporisevic@gmail.com>
2021-12-12 12:28:11 +0100

    Verzija 2.0

--- src/test/tablicaMnozenja.java
+++ src/test/tablicaMnozenja.java
@@ -3,22 +3,30 @@ package test;
 public class tablicaMnozenja {
     public static void main(String[] args) {
         System.out.println("Vježba - Tablica množenja");
-        for (int okvir=0; okvir<25; okvir++) {
+        for (int okvir = 0; okvir < 25; okvir++) {
             System.out.print("*");
         }
         System.out.println();
         System.out.println("#\t\t1\t\t2\t\t3");
-        for (int okvir=0; okvir<25; okvir++) {
+        for (int okvir = 0; okvir < 25; okvir++) {
             System.out.print("*");
         }
         System.out.println();
-        int redoslijed;
-        int[] tablica1 = {0,1,2,3,4,5,6,7,8,9,10};
-        int[] tablica2 = {0,2,4,6,8,10,12,14,16,18,20};
-        int[] tablica3 = {0,3,6,9,12,15,18,21,24,27,30};
+        int[] tabela = {1, 2, 3};
+        int[] redoslijed = {0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        for (redoslijed=1; redoslijed<11; redoslijed++) {
-            System.out.println(redoslijed + "\t\t" + tablica1[redoslijed] + "\t\t" + tablica2[redoslijed] + "\t\t" + tablica3[redoslijed]);
+        for (int prvaTabela=0; prvaTabela<tabela[0]; prvaTabela++) {
+            for (int drugaTabela=1; drugaTabela<tabela[1]; drugaTabela++){
+                for (int trecaTabela=2; trecaTabela<tabela[2]; trecaTabela++){
+                    for (int i=1; i<redoslijed.length; i++) {
+                        int rezultat0 = tabela[prvaTabela] * redoslijed[i];
+                        int rezultat1 = tabela[drugaTabela] * redoslijed[i];
+                        int rezultat2 = tabela[trecaTabela] * redoslijed[i];
+                        System.out.println(i + "\t\t" + rezultat0 + "\t\t" + rezultat1 + "\t\t" + rezultat2);
+                    }
+                }
+            }
         }
+
     }
 }
