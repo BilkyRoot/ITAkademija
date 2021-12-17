1ad0f15a4594a008a7b7b3c50a95fa3fbfb0cb85
BilkyRoot <bilalsporisevic@gmail.com>
2021-12-17 23:38:56 +0100

    Verzija 3.0

--- src/test/dvoDimenzionalniNiz.java
+++ src/test/dvoDimenzionalniNiz.java
@@ -1,9 +1,23 @@
 package test;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class dvoDimenzionalniNiz {
     public static void main(String[] args) {
+
+        // NIZ NAPRAVLJEN POMOĆU RANDOM GENERATORA
+
+        Random generator = new Random();
+
+        int[][] niz = new int[3][5];
+
+        for (int i = 0; i< niz.length; i++){
+            int[] kolona = niz[i];
+            for (int j=0; j< kolona.length; j++){
+                kolona[j] = generator.nextInt(300);
+            }
+        }
 
         // UVODNA PORUKA
 
@@ -26,14 +40,6 @@ public class dvoDimenzionalniNiz {
             System.out.print("-");
         }
         System.out.println();
-
-        // NIZ
-
-        int[][] niz = {
-                {15, 20, 30, 50, 665},
-                {33, 233, 45, 42, 232},
-                {37, 46, 47, 80, 90}
-        };
 
         // POČETNA BOOLEAN VRIJEDNOST
 
